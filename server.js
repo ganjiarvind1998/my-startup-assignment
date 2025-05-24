2bfd298b2c7311ae0f4fd08098c8508e0bfb1aeb
Ganji Arvind <ganjiarvind1998@gmail.com>
2025-05-24 16:48:48 +0530

    backend devolped

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const rateLimiter = require('./middleware/rateLimiter');
+const Message = require("./message_model");
+const Database = require('./database');
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+Database()
+
+app.post('/api/feedback',rateLimiter, (req, res) => {
+    try {
+        const { name, email, message } = req.body
+        if(!name || !email || !message){
+            return res.status(400).json({message:"all fieldes are required "})
+        }
+        const newMessage = new Message({
+            name, email, message
+        })
+        newMessage.save()
+        return res.status(201).json({ message: "feedback sent successfully", newMessage })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message:"server errror", error:error.message})
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
